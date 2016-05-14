1c08330453fa3317412bd9bed2e735d7e483e78d
srabeeh <srabeeh@larci.com>
2016-05-13 21:55:51 -0600

    Basic job processing with net framework concurrency

--- NetConcurrency/Program.cs
+++ NetConcurrency/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using JobQueue;
 
 namespace NetConcurrency
 {
@@ -11,11 +14,42 @@ namespace NetConcurrency
     // ConcurrentStack<T>
     // ConcurrentBag<T> - Unordered collection.
 
+
+    /// <summary>
+    /// No locking is performed and Dequeue is replaced with TryDequeue
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            ConcurrentQueue<Job> pendingJobs = new ConcurrentQueue<Job>();
+            for (int jobId = 0; jobId < 10000; jobId++)
+            {
+                pendingJobs.Enqueue(new Job(jobId));
+            }
 
+            Thread[] processingThreads = new Thread[2];
+
+            ThreadStart runJobs = delegate()
+            {
+                Job job = null;
+
+                while (pendingJobs.TryDequeue(out job))
+                {
+                    job.Process();
+                }
+            };
+
+            for (int i = 0; i < processingThreads.Length; i++)
+            {
+                processingThreads[i] = new Thread(runJobs);
+                processingThreads[i].Start();
+            }
+
+            foreach (Thread thread in processingThreads)
+            {
+                thread.Join();
+            }
         }
     }
 }
