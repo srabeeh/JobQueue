62751e77a1307a8c8449f960f837dab2aa4e9f14
srabeeh <srabeeh@larci.com>
2016-05-13 19:27:09 -0600

    Initial Job class

---
+++ JobQueue/Job.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JobQueue
+{
+    class Job
+    {
+        private readonly int _jobId;
+        private bool _processed;
+        private readonly object _synclock = new object();
+
+
+        public Job(int jobId)
+        {
+            _jobId = jobId;
+        }
+
+        public void Process()
+        {
+            lock (_synclock)
+            {
+                if (_processed)
+                {
+                    throw new InvalidOperationException($"Job {_jobId} executed multiple times");
+                }
+
+                Thread.SpinWait(1000); // Execute thread mimick
+                _processed = true;
+            }
+        }
+    }
+}
